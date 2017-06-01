f8fe17399988e72c0445a0e27c1e642d2c00b13a
AlexTryjan <aet41@case.edu>
2017-05-31 22:07:02 -0500

    farmer options
    Signed-off-by: AlexTryjan <aet41@case.edu>

--- lunchbot.py
+++ lunchbot.py
@@ -9,13 +9,13 @@ key = os.environ.get('SLACK_API_KEY', None)
 
 slack = Slacker(key)
 
-lunch_poll = 'React with your lunch choice, or else:' \
+lunch_poll = 'React with your lunch choice, or else:\n' \
              '1. Cass\n' \
              '2. King\'s Cross\n' \
              '3. %s\n' \
              '4. Spit & Fire\n' \
              '5. Mikey\'s\n' \
-             '6. Is Chopstick\'s really even open?' % (farm_names[0])
+             '6. Is Chopstick\'s really even open?' % (farm_names[randint(0,5)])
 
 # Send a message to #botsgonewild channel
 slack.chat.post_message('#botsgonewild', lunch_poll, as_user=True)
