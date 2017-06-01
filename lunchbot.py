7f8cea9ca187ad6672d836c422f15c32a4d08cae
AlexTryjan <aet41@case.edu>
2017-05-31 22:00:00 -0500

    updated lunch_poll
    Signed-off-by: AlexTryjan <aet41@case.edu>

--- lunchbot.py
+++ lunchbot.py
@@ -1,9 +1,21 @@
 from slacker import Slacker
 import os
 
+farm_names = ['Farmers are friends, not food','Farm to fork',
+              'Farmer Land','tekraM s\'remraF','Old MacDonald\'s',
+              'Real talk, does Judy have a farm fetish?']
+
 key = os.environ.get('SLACK_API_KEY', None)
 
 slack = Slacker(key)
 
+lunch_poll = 'React with your lunch choice, or else:' \
+             '1. Cass\n' \
+             '2. King\'s Cross\n' \
+             '3. %s\n' \
+             '4. Spit & Fire\n' \
+             '5. Mikey\'s\n' \
+             '6. Is Chopstick\'s really even open?' % (farm_names[0])
+
 # Send a message to #botsgonewild channel
-slack.chat.post_message('#botsgonewild', 'Hello fellow robots!', as_user=True)
+slack.chat.post_message('#botsgonewild', 'lunch_poll', as_user=True)
