3d41e1dc4b55ab8b392cff4e2f2241802500635c
AlexTryjan <aet41@case.edu>
2017-06-01 23:29:54 -0500

    Moar farm names
    Signed-off-by: AlexTryjan <aet41@case.edu>

--- lunchbot.py
+++ lunchbot.py
@@ -4,7 +4,17 @@ import os
 
 farm_names = ['Farmers are friends, not food','Farm to fork',
               'Farmer Land','tekraM s\'remraF','Old MacDonald\'s',
-              'Real talk, does Judy have a farm fetish?']
+              'Real talk, does Judy have a farm fetish?','Farmfefe',
+              'Goat Grotto','Sheep Sanctuary','Cow Coliseum',
+              'Farm me, Daddy','To farm or not to farm','Farm-fetched',
+              'EIEIO','[Insert Farm Name Here]','You guys don\'t pay me enough for this',
+              'Goat Land','farm plz, we never go there','tbh the farm is pretty bad',
+              'Pirates of the Farm: Dead Farmers Tell No Tales',
+              'Wake up sheeple, the cow bikes are just farm propaganda',
+              'F-A-R-M','https://www.youtube.com/watch?v=RVJbKPW3Crs','George Orwell',
+              'Boycott the farm!']
+
+farm_index = len(farm_names) - 1
 
 key = os.environ.get('SLACK_API_KEY', None)
 
@@ -16,8 +26,7 @@ lunch_poll = 'React with your lunch choice, or else:\n' \
              ':three: %s\n' \
              ':four: Spit & Fire\n' \
              ':five: Mikey\'s\n' \
-             ':six: Is Chopstick\'s really even open?' % (farm_names[randint(0,5)])
-print(lunch_poll)
+             ':six: Is Chopstick\'s really even open?' % (farm_names[randint(0,farm_index)])
 
 # Send a message to #botsgonewild channel
 slack.chat.post_message('#food', lunch_poll, as_user=True)
