15492086ce065eabf181f4c9080a620895c46928
Alexander Tryjankowski <aet41@case.edu>
2017-07-05 08:44:26 -0500

    Update lunchbot.py

--- lunchbot.py
+++ lunchbot.py
@@ -24,12 +24,11 @@ slack = Slacker(key)
 lunch_poll = 'React with your lunch choice emoji, or else:\n' \
              ':peach: Cass\n' \
              ':crossed_swords: King\'s Cross\n' \
-             ':fire: Wait, there was work today?'
-             #\n' \ ':sheeple: %s\n' \
-             #':fire: Spit & Fire\n' \
-             #':bread: Mikey\'s\n' \
-             #':sushi: Is Chopstick\'s really even open?' % (farm_names[randint(0,farm_index)])
+             ':sheeple: %s\n' \
+             ':fire: Spit & Fire\n' \
+             ':bread: Mikey\'s\n' \
+             ':sushi: Is Chopstick\'s really even open?' % (farm_names[randint(0,farm_index)])
 
-#if(datetime.datetime.today().weekday() < 5) :
+if(datetime.datetime.today().weekday() < 5) :
   # Send a message to #botsgonewild channel
-  #slack.chat.post_message('#food', lunch_poll, as_user=True)
+  slack.chat.post_message('#food', lunch_poll, as_user=True)
